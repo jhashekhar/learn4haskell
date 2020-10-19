fc778b012a15181d171000ba20cbb067ae3f3dbd
jhashekhar <shekhar09jha@gmail.com>
2020-10-19 19:11:15 +0530

    Fixed list output

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -201,7 +201,7 @@ to guess first, what you will see.
 [1,2,3,4,5,6,7,8,9,10]
 
 >>> [10 .. 1]
-[10,9,8,7,6,5,4,3,2,1]
+[]
 
 >>> [10, 9 .. 1]  -- backwards list with explicit step
 [10,9,8,7,6,5,4,3,2,1]
